db2b4dc9975d8bc68eb063948419dfb63fd539c4
Awonke Mnotoza <93478189+Awonke11@users.noreply.github.com>
2022-12-24 19:16:49 +0200

    Color UI

--- components/HomeDisplay.tsx
+++ components/HomeDisplay.tsx
@@ -4,7 +4,7 @@ const HomeDisplay : React.FC = () : JSX.Element => {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Cure your <br /> boredom today</h1>
-            <p className={styles.description}>Having nothing to do, well we always having something for you. View an activity generated for    you. <b>Not happy?</b> No problem, generate a new one .</p>
+            <p className={styles.description}>Having nothing to do, well we always having something for you. View an activity generated for    you. <b className={styles.bold}>Not happy?</b> No problem, generate a new one .</p>
             <button 
                 className={styles.button}
                 onClick={() => location.reload()}
